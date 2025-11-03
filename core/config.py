1dda6d9cd3b61f531c0e065267c5e383b650a31f
Sai Pendli <pendlisai@gmail.com>
2025-11-02 20:20:16 -0800

    Updating url links and adding Design Tools, Project Management Tools competitor sets

--- core/config.py
+++ core/config.py
@@ -11,26 +11,157 @@ COMPETITOR_SETS = {
         {
             "name": "Mixpanel",
             "sources": [
-                {"url": "https://mixpanel.com/blog/", "type": "html"},
-                # {"url": "https://mixpanel.com/blog/feed/", "type": "rss"},
-                # {"url": "https://mixpanel.com/changelog/", "type": "changelog"}
+                {"url": "https://mixpanel.com/blog/category/analytics/", "type": "html"},
+                {"url": "https://mixpanel.com/blog/category/builders/", "type": "html"},
+                {"url": "https://mixpanel.com/blog/category/how-to-build/", "type": "html"},
+                {"url": "https://mixpanel.com/blog/category/inside-mixpanel/", "type": "html"},
             ]
         },
         {
             "name": "Amplitude",
             "sources": [
-                {"url": "https://amplitude.com/blog", "type": "html"},
-                # {"url": "https://amplitude.com/releases", "type": "changelog"}
+                {"url": "https://amplitude.com/blog/category/perspectives/", "type": "html"},
+                {"url": "https://amplitude.com/blog/category/best-practices/", "type": "html"},
+                {"url": "https://amplitude.com/blog/category/inside-amplitude/", "type": "html"},
+                {"url": "https://amplitude.com/blog/category/customer-stories/", "type": "html"},
+                {"url": "https://amplitude.com/blog/authors/", "type": "html"},
             ]
         },
         {
             "name": "Heap",
             "sources": [
-                {"url": "https://www.heap.io/blog", "type": "html"},
-                # {"url": "https://heap.io/changelog", "type": "changelog"}
+                {"url": "https://www.heap.io/blog/", "type": "html"},
+            ]
+        },
+        {
+            "name": "Twilio",
+            "sources": [
+                {"url": "https://www.twilio.com/en-us/blog/", "type": "html"},
+                {"url": "https://www.twilio.com/en-us/blog/developers/", "type": "html"},
+                {"url": "https://www.twilio.com/en-us/blog/insights/", "type": "html"},
+                {"url": "https://www.twilio.com/en-us/blog/products/", "type": "html"},
+                {"url": "https://www.twilio.com/en-us/blog/events/", "type": "html"},
+                {"url": "https://www.twilio.com/en-us/blog/partners/", "type": "html"},
+                {"url": "https://www.twilio.com/en-us/blog/company/", "type": "html"},
+
+            ]
+        },
+        {
+            "name": "Pendo",
+            "sources": [
+                {"url": "https://www.pendo.io/pendo-blog/", "type": "html"},
+                {"url": "https://www.pendo.io/pendo-blog/best-practices/", "type": "html"},
+                {"url": "https://www.pendo.io/pendo-blog/perspectives/", "type": "html"},
+                {"url": "https://www.pendo.io/pendo-blog/profiles/", "type": "html"},
+                {"url": "https://www.pendo.io/pendo-blog/news/", "type": "html"},
+            ]
+        }
+    ],
+
+    "Design Tools":[
+        {
+            "name": "Figma",
+            "sources": [
+                {"url": "https://www.figma.com/blog/product-updates/", "type": "html"},
+                {"url": "https://www.figma.com/blog/product-management/", "type": "html"},
+                {"url": "https://www.figma.com/blog/marketing/", "type": "html"},
+                {"url": "https://www.figma.com/blog/events/", "type": "html"},
+                {"url": "https://www.figma.com/blog/ai/", "type": "html"},
+            ]
+        },
+        {
+            "name": "Sketch",
+            "sources": [
+                {"url": "https://www.sketch.com/blog/learn-design/", "type": "html"},
+                {"url": "https://www.sketch.com/blog/inside-sketch/", "type": "html"},
+                {"url": "https://www.sketch.com/blog/latest-features/", "type": "html"},
+                {"url": "https://www.sketch.com/blog/made-with-sketch/", "type": "html"},
+                {"url": "https://www.sketch.com/blog/feed.xml/", "type": "rss"},
+            ]
+        },
+        {
+            "name": "Framer",
+            "sources" : [
+                {"url": "https://www.framer.com/blog/inspiration", "type": "html"},
+                {"url": "https://www.framer.com/blog/tutorials", "type": "html"},
+                {"url": "https://www.framer.com/blog/resources", "type": "html"},
+                {"url": "https://www.framer.com/blog/news", "type": "html"},
+                {"url": "https://www.framer.com/updates", "type": "html"},
+            ]
+        },
+        {
+            "name": "Canva",
+            "sources" : [
+                {"url": "https://www.canva.com/features/", "type": "html"},
+                {"url": "https://www.canva.com/learn/", "type": "html"},
+                {"url": "https://www.canva.com/newsroom/news/", "type": "html"},
+            ]
+        },
+        {
+            "name": "Adobe XD",
+            "sources" : [
+                {"url": "https://blog.adobe.com/en/topics/xd", "type": "html"}
+            ]
+        }
+    ],
+
+    "Project Management": [
+        {
+            "name": "Asana",
+            "sources" : [
+                {"url": "https://asana.com/features", "type": "html"},
+                {"url": "https://asana.com/inside-asana", "type": "html"},
+            ]
+        },
+        {
+            "name": "Monday.com",
+            "sources" : [
+                {"url": "https://monday.com/blog", "type": "html"},
+                {"url": "https://monday.com/blog/product/", "type": "html"},
+                {"url": "https://monday.com/blog/marketing/", "type": "html"},
+                {"url": "https://monday.com/blog/project-management/", "type": "html"},
+            ]
+        },
+        {
+            "name": "ClickUp",
+            "sources" : [
+                {"url": "https://clickup.com/blog/", "type": "html"},
+                {"url": "https://clickup.com/features", "type": "html"},
+                {"url": "https://clickup.com/blog/automation/", "type": "html"},
+                {"url": "https://clickup.com/blog/project-management/", "type": "html"},
+                {"url": "https://clickup.com/blog/feature-releases/", "type": "html"},
+                {"url": "https://clickup.com/blog/announcements/", "type": "html"},
+                {"url": "https://clickup.com/blog/events/", "type": "html"},
+                {"url": "https://clickup.com/blog/press-releases/", "type": "html"},
+            ]
+        },
+        {
+            "name": "Notion",
+            "sources" : [
+                {"url": "https://www.notion.com/blog", "type": "html"},
+                {"url": "https://www.notion.com/blog/topic/notion-hq", "type": "html"},
+                {"url": "https://www.notion.com/blog/topic/tech", "type": "html"},
+                {"url": "https://www.notion.com/blog/topic/inspiration", "type": "html"},
+            ]
+        },
+        {
+            "name": "Airtable",
+            "sources" : [
+                {"url": "https://blog.airtable.com/", "type": "html"},
+                {"url": "https://blog.airtable.com/tag/product-updates/", "type": "html"},
+                {"url": "https://blog.airtable.com/tag/tips-and-tricks/", "type": "html"},
+            ]
+        },
+        {
+            "name": "Trello",
+            "sources" : [
+                {"url": "https://www.atlassian.com/blog/trello", "type": "html"},
+                {"url": "https://www.atlassian.com/blog/artificial-intelligence", "type": "html"},
+                {"url": "https://www.atlassian.com/blog/project-management", "type": "html"},
             ]
         }
     ]
+
 }
 
 EVENT_CATEGORIES = {
@@ -83,7 +214,7 @@ LLM_CONFIG = {
     "temperature": 0,
     "max_tokens": 500,
     "batch_size": 5,
-    "confidence_threshold": 0.1
+    "confidence_threshold": 0.5
 }
 
 def get_all_competitors() -> List[str]:
